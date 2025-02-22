3543d5e00921134f33c34329d8cee7b9d906e117
sumdx <sumdas74@gmail.com>
2025-02-21 20:18:07 -0800

    project initiated

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 3000;
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Home
+app.get('/',(req,res) =>{
+    res.send("Task Management Server is Running");
+})
+
+app.post('/users',(req,res) =>{
+    const user = req.body;
+    console.log(req.body);
+})
+
+app.listen(port, ()=>{
+    console.log(`Task Mangement is running`)
+})
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.g7l6o.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
+
+  async function run() {
+    try {
+      // Connect the client to the server	(optional starting in v4.7)
+      await client.connect();
+
+
+      
+      // Send a ping to confirm a successful connection
+      await client.db("admin").command({ ping: 1 });
+      console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+      // Ensures that the client will close when you finish/error
+      await client.close();
+    }
+  }
+run().catch(console.dir);
